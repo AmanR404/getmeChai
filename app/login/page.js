d8b68d5e10d680d8a2292146916e4550c795d17d
AmanR404 <107179665+AmanR404@users.noreply.github.com>
2024-11-13 14:37:53 +0530

    Added info

--- app/login/page.js
+++ app/login/page.js
@@ -4,9 +4,23 @@ import { useSession,signIn, signOut } from "next-auth/react"
 import Link from 'next/link'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react';
 
 const login = () => {
   const { data: session } = useSession()
+  useEffect(() => {
+    toast.success('Only Github Provider is Added!', {
+      position: "bottom-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark"
+      });
+  }, [])
+  
   if(session){
     toast.success('Logged In!', {
       position: "bottom-left",
@@ -36,6 +50,18 @@ const login = () => {
   }
   return (
     <div>
+    <ToastContainer
+                position="bottom-left"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss={false}
+                draggable
+                pauseOnHover
+                theme="dark"
+               />
         <h1 className="font-bold text-white text-3xl text-center mb-10">Login to Get your fans to support you</h1>
         <div className='text-gray-900 flex flex-col gap-2 justify-center items-center'>
             <button className='bg-white w-60 flex justify-center gap-2 items-center rounded-md py-1 hover:bg-gray-300'><img width={25} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5Nhdac4WogJcyclKl4W0SNeUBj7q-hBCAWg&s" alt="" /><span>Continue with Google</span></button>
