4a386fe71855c157d3480801ff2b5a4ce1ecb92a
AmanR404 <107179665+AmanR404@users.noreply.github.com>
2024-11-28 11:27:44 +0530

    Add files via upload

--- app/login/page.js
+++ app/login/page.js
@@ -6,7 +6,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect } from 'react';
 
-const login = () => {
+const Login = () => {
   const { data: session } = useSession()
   useEffect(() => {
     toast.success('Only Github Provider is Added!', {
@@ -64,10 +64,10 @@ const login = () => {
                />
         <h1 className="font-bold text-white text-3xl text-center mb-10">Login to Get your fans to support you</h1>
         <div className='text-gray-900 flex flex-col gap-2 justify-center items-center'>
-            <button className='bg-white w-60 flex justify-center gap-2 items-center rounded-md py-1 hover:bg-gray-300'><img width={25} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5Nhdac4WogJcyclKl4W0SNeUBj7q-hBCAWg&s" alt="" /><span>Continue with Google</span></button>
-            <button className='bg-white w-60 flex justify-center gap-1 items-center rounded-md py-1 hover:bg-gray-300'><img width={25} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXu0p5xl_tB4Id8caYETptjtbTevOIU1vXOA&s" alt="" /><span>Continue with LinkedIn</span></button>
+            <button className='bg-white w-60 flex justify-center gap-2 items-center rounded-md py-1 hover:bg-gray-300'><img width={25} src="https://encrypted-tbn0.gstatic.com/imgs?q=tbn:ANd9GcT5Nhdac4WogJcyclKl4W0SNeUBj7q-hBCAWg&s" alt="" /><span>Continue with Google</span></button>
+            <button className='bg-white w-60 flex justify-center gap-1 items-center rounded-md py-1 hover:bg-gray-300'><img width={25} src="https://encrypted-tbn0.gstatic.com/imgs?q=tbn:ANd9GcRXu0p5xl_tB4Id8caYETptjtbTevOIU1vXOA&s" alt="" /><span>Continue with LinkedIn</span></button>
             <button className='bg-white w-60 flex justify-center gap-[11px] items-center rounded-md py-1 hover:bg-gray-300'><img width={25} src="https://cdn2.iconfinder.com/data/icons/minimalism/512/twitter.png" alt="" /><span>Continue with Twitter</span></button>
-            <button className='bg-white w-60 flex justify-center gap-1 items-center rounded-md py-1 hover:bg-gray-300'><img width={25} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtfbcYeYgf0wQJ-LSPm3CPbyB7T1p0f5bnaA&s" alt="" /><span>Continue with Facebook</span></button>
+            <button className='bg-white w-60 flex justify-center gap-1 items-center rounded-md py-1 hover:bg-gray-300'><img width={25} src="https://encrypted-tbn0.gstatic.com/imgs?q=tbn:ANd9GcTtfbcYeYgf0wQJ-LSPm3CPbyB7T1p0f5bnaA&s" alt="" /><span>Continue with Facebook</span></button>
             <button onClick={()=>{signIn("github")}} className='bg-white w-60 flex justify-center gap-3 items-center rounded-md py-1 hover:bg-gray-300'><img width={25} src="https://thumbs.dreamstime.com/b/astana-kazakhstan-july-github-icon-logo-vector-symbol-198642058.jpg" alt="" /><span>Continue with Github</span></button>
             <button className='bg-white w-60 flex justify-center items-center gap-3.5 rounded-md py-1 hover:bg-gray-300'><img width={25} src="https://img.icons8.com/m_rounded/512/mac-os.png" alt="" /><span>Continue with Apple</span></button>
         </div>
@@ -75,4 +75,4 @@ const login = () => {
   )
 }
 
-export default login
+export default Login
